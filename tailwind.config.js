300cab9a2cfe43528dbab0736d7ddb4bb8a30d31
rzmk <mueez25@gmail.com>
2023-01-27 23:26:40 -0500

    :wrench: Remove deprecated purge from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  purge: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
   plugins: [],
 };
